cbb1b41450baf2a04969322708c543b0c6e32803
Anand Gupta <guptaanand294@gmail.com>
2020-09-17 19:54:30 +0530

    first commit

---
+++ app/src/main/java/com/example/android/qrcodescanmlkit/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.android.qrcodescanmlkit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String[] PERMISSIONS = new String[] {
+            Manifest.permission.CAMERA,
+            "android.permission.CAMERA",
+    };
+
+    Camera camera;
+    Preview preview;
+    PreviewView cameraView;
+    CameraSelector cameraSelector;
+    ImageAnalysis imageAnalysis;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //We are asking for permission here
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)== PermissionChecker.PERMISSION_GRANTED){
+            startCamera();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,PERMISSIONS,101);
+        }
+    }
+
+    //Checking Permissions are accepted by user or not
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)== PermissionChecker.PERMISSION_GRANTED){
+            startCamera();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,PERMISSIONS,101);
+            Toast.makeText(this, "Please accept the required permissions", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void startCamera() {
+        //Camera hardware is binded with our application software with CameraProvider
+        Toast.makeText(this,"Permission granted",Toast.LENGTH_SHORT).show();
+        cameraView = findViewById(R.id.cameraView);
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(cameraView.createSurfaceProvider());
+        cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        imageAnalysis=new ImageAnalysis.Builder().setTargetResolution(new Size(1280,720)).setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this),new YourAnalyzer());
+        cameraProvider.unbindAll();
+        camera = cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageAnalysis);
+    }
+
+    //Own Custom class
+    private class YourAnalyzer implements ImageAnalysis.Analyzer {
+
+        @Override
+        public void analyze(ImageProxy imageProxy) {
+            @SuppressLint("UnsafeExperimentalUsageError") Image mediaImage = imageProxy.getImage();
+            if (mediaImage != null) {
+                InputImage image =
+                        InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                // Pass image to an ML Kit Vision API
+                // ...
+                BarcodeScannerOptions options=new BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE,Barcode.FORMAT_AZTEC).build();
+                BarcodeScanner scanner = BarcodeScanning.getClient(options);
+                Task<List<Barcode>> result = scanner.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                            @Override
+                            public void onSuccess(List<Barcode> barcodes) {
+                                // Task completed successfully
+                                // ...
+                                Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT);
+                                for (Barcode barcode: barcodes) {
+                                    Rect bounds = barcode.getBoundingBox();
+                                    Point[] corners = barcode.getCornerPoints();
+
+                                    String rawValue = barcode.getRawValue();
+
+                                    int valueType = barcode.getValueType();
+                                    // See API reference for complete list of supported types
+                                    switch (valueType) {
+                                        case Barcode.TYPE_WIFI:
+                                            String ssid = barcode.getWifi().getSsid();
+                                            String password = barcode.getWifi().getPassword();
+                                            int type = barcode.getWifi().getEncryptionType();
+                                            Toast.makeText(getApplicationContext(),ssid,Toast.LENGTH_LONG).show();
+                                            Log.i("Barcode Type Wifi","Success");
+                                            break;
+                                        case Barcode.TYPE_URL:
+                                            String title = barcode.getUrl().getTitle();
+                                            String url = barcode.getUrl().getUrl();
+                                            Log.i("Barcode Type URL","Success");
+                                            Toast.makeText(getApplicationContext(),url,Toast.LENGTH_LONG).show();
+                                            break;
+                                    }
+
+                                }
+                                imageProxy.close();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                                Log.i("FailureListener","Failure caught");
+                                Toast.makeText(getApplicationContext(),"Failure",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+            }
+
+
+        }
+    }
+
+
+}
